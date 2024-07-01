38199b45ad564d09d5973cddaac9df650307a655
JorgeGarciaC <jorgegarciatldc@hotmail.com>
2024-07-01 15:04:59 +0200

    Update 1_xpehh_YRIvsCEU.sh

--- Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_xpehh_YRIvsCEU.sh
+++ Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_xpehh_YRIvsCEU.sh
@@ -1,14 +1,5 @@
 #!/bin/bash     
 
-# define names
-#SBATCH --job-name=selscan
-#SBATCH --error selscan-%j.err
-#SBATCH --output selsan-%j.out 
-
-# memory and CPUs request
-#SBATCH --mem=6G
-#SBATCH --cpus-per-task=8
-
 # XPEHH computation for YRI vs CEU
 HAP1=data/YRI/YRI.recode.vcf.hap
 HAP2=data/CEU/CEU.recode.vcf.hap
