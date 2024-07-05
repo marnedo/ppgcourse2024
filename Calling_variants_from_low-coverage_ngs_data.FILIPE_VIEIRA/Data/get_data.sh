a4c089e299e501e610637d065c1c26cf84b6271a
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-07-05 12:57:29 +0200

    Update to download example data

--- Calling_variants_from_low-coverage_ngs_data.FILIPE_VIEIRA/Data/get_data.sh
+++ Calling_variants_from_low-coverage_ngs_data.FILIPE_VIEIRA/Data/get_data.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
-wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz.fai
+REG="11:20000000-23000000"
 
-cut -f 1,2 samples.annot | tail -n +2 | parallel --dryrun --colsep "\t" -j 10 "wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/{1}/alignment/{1}.chrom11.ILLUMINA.bwa.{2}.*" | bash
-touch *.bai
- 
-ls *.bam | sort -t "." -k 5,5 > samples.bam_list
+samtools faidx http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz $REG > hs37d5.fa.gz
+samtools faidx hs37d5.fa.gz
 
+parallel --header : "samtools view -o {sample}.{pop}.bam http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/{1}/alignment/{sample}.chrom11.ILLUMINA.bwa.{pop}.low_coverage.{date}.bam $REG" :::: samples.annot
+rm *.bai
+parallel samtools index ::: *.bam
+
+ls *.bam | sort -t "." -k 2,2 > samples.bam_list
