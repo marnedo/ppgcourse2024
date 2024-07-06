8cada2916a957b15a4762ec6a16b0af6a7c32438
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2024-07-06 16:53:38 +0200

    Update get_data.sh

--- Calling_variants_from_low-coverage_ngs_data.FILIPE_VIEIRA/get_data.sh
+++ Calling_variants_from_low-coverage_ngs_data.FILIPE_VIEIRA/get_data.sh
@@ -2,7 +2,7 @@
 
 export REG="11:20000000-23000000"
 
-samtools faidx http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz $REG > hs37d5.fa.gz
+samtools faidx http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz 11 > hs37d5.fa.gz
 samtools faidx hs37d5.fa.gz
 
 #parallel --header : "samtools view -o {sample}.{pop}.bam http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/{1}/alignment/{sample}.chrom11.ILLUMINA.bwa.{pop}.low_coverage.{date}.bam $REG" :::: samples.annot
