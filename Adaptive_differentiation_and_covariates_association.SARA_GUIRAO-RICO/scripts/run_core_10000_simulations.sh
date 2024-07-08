2d6861405c77c745cb33690cb7a896a4fca58d9f
sguir <52529085+sguir@users.noreply.github.com>
2024-07-08 18:57:10 +0200

    Update run_core_10000_simulations.sh

--- Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_core_10000_simulations.sh
+++ Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_core_10000_simulations.sh
@@ -2,4 +2,4 @@
 
 mkdir simulations_core                                                                                                                        
 # run BayPass (CORE Model) with the 10,000 PODs as input
-../software/baypass_public/sources/g_baypass -npop 52 -gfile ./results_core/G.hgdp_pods_1000 -outprefix ./simulations_core/hgdp_pod_1000
+../software/baypass_public/sources/g_baypass -npop 52 -gfile ./results_core/G.hgdp_pods_10000 -outprefix ./simulations_core/hgdp_pod_10000
