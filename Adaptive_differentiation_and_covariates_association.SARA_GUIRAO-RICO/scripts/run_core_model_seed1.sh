a2f86f5041bced748ac80547b99ac4036d8dfd95
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2024-07-08 17:01:04 +0200

    Update run_core_model_seed1.sh

--- Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_core_model_seed1.sh
+++ Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_core_model_seed1.sh
@@ -1,21 +1,5 @@
-#!/bin/bash                                                                                                             
-
-# define names                                                                                                          
-#SBATCH --job-name=bp_core_seed1                                                                                         
-#SBATCH --error bp_core_seed1-%j.err                                                                                     
-#SBATCH --output bp_core_seed1-%j.out                                                                                    
-
-# memory and CPUs request                                                                                               
-#SBATCH --mem=6G                                                                                                        
-#SBATCH --cpus-per-task=8 
-
-# directories
-INPUT=../input
-cd $INPUT
-
-# module load                                                                                                           
-module load BayPass   
+#!/bin/bas
 
 # run BayPass (CORE Model) with seed1
-g_baypass -npop 52 -gfile hgdp.geno -nthreads 8 -seed 15263 -outprefix hgdp_core_s1
-
+mkdir results_core
+../software/baypass_public/sources/g_baypass -npop 52 -gfile ./input/hgdp.geno -seed 15263 -outprefix ./results_core/hgdp_core_s1
