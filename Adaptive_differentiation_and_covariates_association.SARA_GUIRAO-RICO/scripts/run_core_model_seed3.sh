936d1b6c0914da619f57922a0369b8aa16f76b82
sguir <52529085+sguir@users.noreply.github.com>
2024-07-08 17:07:20 +0200

    Update run_core_model_seed3.sh

--- Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_core_model_seed3.sh
+++ Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_core_model_seed3.sh
@@ -1,20 +1,6 @@
-#!/bin/bash                                                                                                             
-
-# define names                                                                                                          
-#SBATCH --job-name=bp_core_seed3                                                                                         
-#SBATCH --error bp_core_seed3-%j.err                                                                                     
-#SBATCH --output bp_core_seed3-%j.out                                                                                    
-
-# memory and CPUs request                                                                                               
-#SBATCH --mem=6G                                                                                                        
-#SBATCH --cpus-per-task=8 
-
-# directories
-INPUT=../input
-cd $INPUT
-
-# module load                                                                                                           
-module load BayPass   
+#!/bin/bash
 
 # run BayPass (CORE Model) with seed3
-g_baypass -npop 52 -gfile hgdp.geno -nthreads 8 -seed 94875 -outprefix hgdp_core_s3
+mkdir results_core
+../software/baypass_public/sources/g_baypass -npop 52 -gfile ./input/hgdp.geno -seed 94875 -outprefix ./results_core/hgdp_core_s3
+
