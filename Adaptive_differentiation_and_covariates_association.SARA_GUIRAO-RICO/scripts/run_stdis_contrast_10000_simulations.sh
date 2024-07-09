3c7ade6bbf9e46458641c087c59761fdd1cf0e64
sguir <52529085+sguir@users.noreply.github.com>
2024-07-09 22:23:40 +0200

    Update run_stdis_contrast_10000_simulations.sh

--- Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_contrast_10000_simulations.sh
+++ Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_contrast_10000_simulations.sh
@@ -1,20 +1,4 @@
 #!/bin/bash                                                                                                             
 
-# define names                                                                                                          
-#SBATCH --job-name=bp_stdis_contr                                                                                         
-#SBATCH --error bp_stdis_contr-%j.err                                                                                     
-#SBATCH --output bp_stdis_contr-%j.out                                                                                    
-
-# memory and CPUs request                                                                                               
-#SBATCH --mem=6G                                                                                                        
-#SBATCH --cpus-per-task=8 
-
-# directories
-INPUT=../input
-cd $INPUT
-
-# module load                                                                                                           
-module load BayPass   
-
 # run BayPass (CORE Model) with the 10000 c2 PODs as input
-g_baypass -npop 52 -gfile G.hgdp_C2_10000_pods -contrastfile covariates_eu -efile covariates_eu -nthreads 8 -outprefix hgdp_contrast_10000_pods
+../software/baypass_public/sources/g_baypass -npop 52 -gfile ./results_stdis_contr/G.hgdp_C2_10000_pods -contrastfile ./input/covariates_eu -efile ./input/covariates_eu -outprefix ./results_stdis_contr/hgdp_contrast_10000_pods 
