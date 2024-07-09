dddc7346bc55b8e20cc6b54d1de0c8b74589627a
sguir <52529085+sguir@users.noreply.github.com>
2024-07-09 21:07:37 +0200

    Update run_stdis_10000_simulations.sh

--- Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_10000_simulations.sh
+++ Adaptive_differentiation_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_10000_simulations.sh
@@ -1,21 +1,4 @@
 #!/bin/bash                                                                                                             
 
-# define names                                                                                                          
-#SBATCH --job-name=bp_stdis_10000_sim                                                                                         
-#SBATCH --error bp_stdis_10000-%j.err                                                                                     
-#SBATCH --output bp_stdis_10000-%j.out                                                                                    
-
-# memory and CPUs request                                                                                               
-#SBATCH --mem=6G                                                                                                        
-#SBATCH --cpus-per-task=8 
-
-# directories
-INPUT=../input
-cd $INPUT
-
-# module load                                                                                                           
-module load BayPass   
-
 # run BayPass (CORE Model) with the 10000 PODs as input
-g_baypass -npop 52 -gfile G.hgdp_pods_10000 -efile covariates -scalecov -nthreads 8 -outprefix hgdp_stdis_10000_pods
- 
+../software/baypass_public/sources/g_baypass -npop 52 -gfile ./results_core/G.hgdp_pods_10000 -efile ./input/covariates -scalecov  -outprefix ./results_standard/hgdp_stdis_10000_pods 
